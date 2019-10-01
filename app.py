3f4e94d78bd0fc518f1bd83050be00ae3228c9b9
gccpacman <gccpacman@gmail.com>
2019-10-01 14:09:25 +0800

    update cdk stack

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from cdk_pycon_flask.cdk_pycon_flask_stack import CdkPyconFlaskStack
 
 
 app = core.App()
-CdkPyconFlaskStack(app, "cdk-pycon-flask")
+CdkPyconFlaskStack(app, "cdk-pycon-flask", env={'account': '959696344829', 'region': 'ap-northeast-2'})
 
 app.synth()
--- cdk_pycon_flask/cdk_pycon_flask_stack.py
+++ cdk_pycon_flask/cdk_pycon_flask_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core
+from aws_cdk import core, aws_ec2, aws_ecs, aws_ecs_patterns
 
 
 class CdkPyconFlaskStack(core.Stack):
@@ -7,3 +7,20 @@ class CdkPyconFlaskStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = aws_ec2.Vpc.from_lookup(self, 'cdkVPC', vpc_id='vpc-06bc80b08769c2824')
+        # vpc = aws_ec2.Vpc(self, 'FargateFlaskVPC', cidr='10.0.0.0/16', nat_gateways=1)
+        
+        # ECS cluster
+        cluster = aws_ecs.Cluster(self, 'Cluster', vpc=vpc)
+        svc = aws_ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, 'FargateService',
+            cluster=cluster,
+            image=aws_ecs.ContainerImage.from_asset('flask-docker-app'),
+            container_port=5000,
+            environment={
+                'PLATFORM': 'AWS Fargate :-)'
+            }
+        )
+
+        core.CfnOutput(self, 'SericeURL', value="http://{}".format(
+            svc.load_balancer.load_balancer_dns_name))
